11821289cecabe4fad42e70beede9d8dee5f2f42
Ian Clester <ijc@ijc8.me>
2023-12-24 00:41:32 -0500

    Scale & chords

--- test.c
+++ test.c
@@ -24,6 +24,7 @@ float m2f(int pitch) {
     return freq;
 }
 
+// TODO: Consider GCC extension of pointers to labels
 #define cr_begin switch (cr->line_number) { case 0:;
 #define cr_end(x) } cr->line_number = -1; return x
 
@@ -57,7 +58,8 @@ cr_vars(env, float t);
 float env(env_state *cr, float dur) {
     cr_begin;
     for (cr->t = 0; cr->t < dur; cr->t += dt) {
-        cr_yield(1 - cr->t / dur);
+        float x = 1 - cr->t / dur;
+        cr_yield(x * x);
     }
     cr_end(0);
 }
@@ -90,27 +92,29 @@ float sleep(sleep_state *cr, float dur) {
 
 #define reset(cr_state) (cr_state.line_number = 0)
 
+const int chords[][3] = {{0, 3, 5}, {1, 2, 5}, {2, 4, 5}, {1, 4, 6}};
+const int scale[] = {0, 2, 4, 5, 7, 9, 11};
+
 float upper() {
     // Let's pretend we're a coroutine!
     // Persistent locals
     static struct {
         int freq;
-        float dur;
         sqr_state sqr;
         env_state env;
-    } notes[] = {{0, 0.5}, {0, 0.25}, {0, 0.25}, {0, 0.5}};
-    static int i, j;
+    } notes[SIZEOF(chords[0])];
+    static int c, i, j;
     static float t;
     static sleep_state sleep_state;
     const float dur = 0.25;
     // Start of function
     scr_begin;
-    for (;;) {
+    for (c = 0;; c = (c + 1) % SIZEOF(chords)) {
         for (int k = 0; k < SIZEOF(notes); k++) {
-            notes[k].freq = m2f(rand() % 13 + 60);
+            notes[k].freq = m2f(scale[chords[c][k]] + 60); // m2f(rand() % 13 + 60);
         }
         for (j = 0; j < 6; j++) {
-            if (rand() % 2 == 0) { // like `degrade`
+            if (j > 0 && rand() % 2 == 0) { // like `degrade`
                 // Sit this one out.
                 reset(sleep_state);
                 scr_yield_from(sleep, sleep_state, dur);
@@ -134,37 +138,28 @@ float upper() {
 
 float lower() {
     // Let's pretend we're a coroutine!
-    // Boilerplate
-    static int state = 0;
-    switch (state) {
-        case 0: goto label0;
-        case 1: goto label1;
-    }
     // Persistent locals
     static float notes[][2] = {{0, 1.0}, {0, 0.5}};
-    static int i, j;
+    static int c, i, j;
     static float t, freq, dur;
     static sqr_state my_sqr = {0};
     static env_state my_env = {0};
     // Start of function
-    label0:
-    for (;;) {
-        for (int k = 0; k < SIZEOF(notes); k++) {
-            notes[k][0] = m2f(rand() % 13 + 36);
-        }
-        for (j = 0; j < 2; j++) {
-            for (i = 0; i < SIZEOF(notes); i++) {
-                freq = notes[i][0];
-                dur = notes[i][1];
-                reset(my_sqr);
-                reset(my_env);
-                for (t = 0; t < dur; t += dt) {
-                    state = 1; return sqr(&my_sqr, freq) * env(&my_env, dur); label1:;
-                }
+    scr_begin;
+    for (c = 0;; c = (c + 1) % SIZEOF(chords)) {
+        notes[0][0] = m2f(scale[chords[c][0]] + 48);
+        notes[1][0] = m2f(scale[chords[c][1]] + 48);
+        for (i = 0; i < SIZEOF(notes); i++) {
+            freq = notes[i][0];
+            dur = notes[i][1];
+            reset(my_sqr);
+            reset(my_env);
+            for (t = 0; t < dur; t += dt) {
+                scr_yield(sqr(&my_sqr, freq) * env(&my_env, dur));
             }
         }
     }
-    return 0;
+    scr_end(0);
 }
 
 #ifdef __EMSCRIPTEN__
